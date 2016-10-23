9cf216cec3053fc37a11bd9029a6d12e00e72e9b
lemonheadhs <744640996@qq.com>
2016-10-23 22:16:26 +0800

    partial commit

---
+++ LepackOne.Site/App_Plugins/LeReport/leReport.controller.js
@@ -0,0 +1,31 @@
+﻿(function () {
+    'use strict';
+
+    function leReportCtrl($scope, leRequestHelper) {
+        var ctrl = this;
+        ctrl.reportFiles = null;
+
+        ctrl.importReportData = importReportData;
+
+        $scope.$on('filesSelected', function (event, args) {
+            ctrl.reportFiles = args.files;
+
+            if (event.stopPropagation) {
+                event.stopPropagation();
+            }
+        });
+
+        function importReportData() {
+            leRequestHelper
+                .postDataFile('', {}, ctrl.reportFiles || [])
+                .succcess(function (data, status, headers, config) {
+
+                })
+                .error();
+        }
+
+    }
+    
+    angular.module('umbraco').controller('leReportCtrl', leReportCtrl)
+
+})();
--- LepackOne.Site/App_Plugins/LeReport/leRequestHelper.service.js
+++ LepackOne.Site/App_Plugins/LeReport/leRequestHelper.service.js
@@ -4,7 +4,31 @@
     function leRequestHelper($http, $q) {
         return {
             postDataFile: function (url, data, files) {
+                return $http({
+                    method: 'POST',
+                    url: url,
+                    headers: { 'Content-type': false },
+                    data: data,
+                    transformRequest: function (data) {
+                        var formData = new FormData();
+                        angular.forEach(data, function (value, key) {
+                            formData.append(key,
+                                !angular.isString(value) ? angular.toJson(value) : value);
+                        });
 
+                        if (angular.isArray(files)) {
+                            angular.forEach(files, function (file, idx) {
+                                formData.append('file_' + idx, file);
+                            });
+                        } else {
+                            angular.forEach(files, function (file, key) {
+                                formData.append(key, file);
+                            });
+                        }
+
+                        return formData;
+                    }
+                });
             }
         }
     }

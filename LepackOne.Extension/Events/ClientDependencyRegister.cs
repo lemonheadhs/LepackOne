a80d5baaf47772595618177f032e917748eb4a68
lemonheadhs <744640996@qq.com>
2016-11-07 17:44:08 +0800

    add usync

---
+++ LepackOne.Extension/Events/ClientDependencyRegister.cs
@@ -0,0 +1,18 @@
+﻿using ClientDependency.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Umbraco.Core;
+
+namespace LepackOne.Extension.Events
+{
+    public class ClientDependencyRegister : ApplicationEventHandler
+    {
+        protected override void ApplicationStarted(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
+        {
+            BundleManager.CreateJsBundle("");
+        }
+    }
+}
